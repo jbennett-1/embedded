cf6e69369719b1283140c07aaa7400db0b237f09
jbennett-1 <juliabennett46@gmail.com>
2022-04-15 18:45:43 -0700

    committing any recent changes

--- makefile
+++ makefile
@@ -5,7 +5,8 @@ OBJDUMP := arm-none-eabi-objdump
 SIZE := arm-none-eabi-size
 
 CFLAGS := -O0 -ffreestanding -fno-pie -fno-stack-protector -g3 -march=armv7-m -mthumb -Wall -mgeneral-regs-only 
-CFLAGS += -I/home/juliabennett/Desktop/embeddedPCA/Include
+CFLAGS += -I/home/juliabennett/Desktop/embedded/Include
+CLFAGS += -I/home/juliabennett/Desktop/embedded/Include/dsp
 
 ODIR := obj
 SDIR := src
--- src/timeOptimization.c
+++ src/timeOptimization.c
@@ -1,47 +1,29 @@
-#include "pca_wrapper.h"
-#include "f2c.h"
-#include "clapack.h"
-#include "fft_wrapper.h"
+#include <stdint.h>
+#include "dsp/transform_functions.h"
 
+//for headers, replace anything fft with the libraries
 void fft_pca(float* input_buffer, float* output_buffer, void* args)
 {
-    struct fft_pca_args* inputs = (struct fft_pca_args*) args;
-    float* fft_buf = inputs->fft_buf;
-    float* cov_mat = inputs->cov_mat;
-    float* cov_mat_means = inputs->cov_mat_means;
-    float* princ_comp = inputs->princ_comp;
-    float* work = inputs->work;
-    uint32_t* iwork = inputs->iwork;
-    uint32_t lwork = inputs->lwork;
-    uint32_t vec_len = inputs->vec_len;
-    uint32_t vec_num = inputs->vec_num;
-
-    fft_obs_matrix(input_buffer, fft_buf,  vec_len, vec_num, inputs->f_args);
-
-    vec_len = (vec_len/2 + 1); // since data is real, vectors after fft are length n/2 + 1 
-
-    cov(fft_buf, cov_mat, cov_mat_means, vec_len, vec_num);
-
-    integer n = vec_len, lda = vec_len, ldz = vec_len;
-    integer lower_limit = vec_len;
-    integer upper_limit = vec_len;
-    integer eig_val_found = 0;
-    integer ifail = 0;
-    integer info = 0;
-    real eig_val = 0;
-    real absol = 0.001;
-//   ssyevx_("Vectors", "Indices", "Upper", (integer*) &n, (real*) cov_mat, (integer*) &lda, (real*) 0, (real*) 0, (integer*) &upper_limit, (integer*) &lower_limit,
-
-//(real*) &absol, (integer*) &eig_val_found, (real*) &eig_val, (real*) princ_comp, (integer*) &ldz, (real*) work, (integer*) &lwork, (integer*) iwork,
-  //         (integer*) &ifail, (integer*) &info);
-
-   // fix_output(princ_comp, vec_len);
-
-   // project_data(fft_buf, princ_comp, output_buffer, vec_len, vec_num);
+	//keep args in functions, args
+	int* input_buf[] = {1, 2, 6, 4, 3, 9, 7, 0, 12, 4, 7, 23, 9, 2, 7 , 3, 1, 3, 7, 1, 2, 9, 2, 1, 4, 5, 3, 1, 8, 2, 1, 1};
+	struct fft_pca_args* inputs = (struct fft_pca_args*) args;
+	
+	//data
+//	float* fft_buf[] = *output_buffer;
+	//output buffer
+	float* fft_buf = inputs->fft_buf;
+	float* cov_mat = inputs->cov_mat;
+	float* cov_mat_means = inputs->cov_mat_means;
+	uint32_t vec_len = inputs->vec_len;
+	uint32_t vec_num = inputs->vec_num;
+	
+	fft_obs_matrix(input_buf, fft_buf,  vec_len, vec_num);
+	vec_len = (vec_len/2 + 1); // since data is real, vectors after fft are length n/2 + 1 
+	cov(fft_buf, cov_mat, cov_mat_means, vec_len, vec_num);
 }
 
 
-void fft_obs_matrix(float* input, float* output, uint32_t vec_len, uint32_t vec_num, struct fft_args* args)
+void fft_obs_matrix(float* input, float* output, uint32_t vec_len, uint32_t vec_num,struct fft_args* args)
 {
     for (int i = 0; i < vec_num; i++)
     {
@@ -59,10 +41,7 @@ void fft_obs_matrix(float* input, float* output, uint32_t vec_len, uint32_t vec_
 void cov(float* A, float* cov_mat, float* means, uint32_t vec_len, uint32_t vec_num)
 {
 
-    calc_means(A, means, vec_len, vec_num);
-
     for (int i = 0; i < vec_len; i++) {
-
         for (int j = i; j < vec_len; j++) {
             // calculate covariance between two vectors
             cov_mat[i*vec_len + j] = 0;
@@ -76,4 +55,26 @@ void cov(float* A, float* cov_mat, float* means, uint32_t vec_len, uint32_t vec_
         }
     }
 }
+}
+/*
+//not specified for code, rework this section out
+struct fft_pca_args* alloc_fft_pca_args(uint32_t vec_len, uint32_t vec_num)
+{
+    struct fft_pca_args* args = malloc(sizeof(struct fft_pca_args));
+    
+    args->f_args = alloc_fft_args(vec_len);
+    args->vec_len = vec_len;
+    args->vec_num = vec_num;
+    args->fft_buf = malloc(sizeof(float)*(vec_len/2 + 1)*vec_num);
+    args->cov_mat = malloc(sizeof(float)*vec_len*vec_len);
+    args->cov_mat_means = malloc(sizeof(float)*vec_len);
 
+    // Allocate correct amount of memory for work
+    int lower_limit = vec_len;
+    int upper_limit = vec_len;
+    args->work = malloc(sizeof(float)*50000);
+
+    return args;
+}
+*/
+//overun buffer before going through fft, adjust for when the buffer changes after it halves

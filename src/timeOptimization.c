09b836f5c69af71063e6fbf860c97e2a675ca445
jbennett-1 <juliabennett46@gmail.com>
2022-05-20 11:37:16 -0700

    fixed pointers for fft_obs

--- src/timeOptimization.c
+++ src/timeOptimization.c
@@ -31,14 +31,13 @@ struct fft_pca_args{
     arm_rfft_fast_instance_f32* fft_data;
 
     uint16_t fft_len;
-    float32_t* fft_p;
     float32_t* fft_out;
     uint8_t ifftFlag;
 
     uint8_t bits;
 };
 
-void initialize(float32_t* data, struct eig_decomp_args* eig_input, struct fft_pca_args* input, float32_t* output_buffer, arm_matrix_instance_f32* matrix_buffer, float32_t* input_data, uint16_t vec_len, uint16_t vec_num, arm_rfft_fast_instance_f32* fft_data, uint16_t fft_len, float32_t* fft_out, uint8_t ifftFlag, float32_t* cov_buffer, float32_t* cov_mat_means, float32_t* eig_buffer, uint8_t bits)
+void initialize(float32_t* tmp, float32_t* data, struct eig_decomp_args* eig_input, struct fft_pca_args* input, float32_t* output_buffer, arm_matrix_instance_f32* matrix_buffer, float32_t* input_data, uint16_t vec_len, uint16_t vec_num, arm_rfft_fast_instance_f32* fft_data, uint16_t fft_len, float32_t* fft_out, uint8_t ifftFlag, float32_t* cov_buffer, float32_t* cov_mat_means, float32_t* eig_buffer, uint8_t bits)
 {
     input->eig_buffer=eig_buffer;
     input->cov_buffer=cov_buffer;
@@ -63,7 +62,7 @@ void initialize(float32_t* data, struct eig_decomp_args* eig_input, struct fft_p
     arm_mat_init_f32(matrix_buffer, vec_len, vec_num, input_data);
 }
 
-void fft_obs_matrix(float32_t* data, arm_rfft_fast_instance_f32* fft_buf, uint16_t vec_len, uint16_t vec_num, struct fft_pca_args* args)
+void fft_obs_matrix(float32_t* data, arm_rfft_fast_instance_f32* fft_data, uint16_t vec_len, uint16_t vec_num, struct fft_pca_args* args)
 {
     for (int16_t i = 0; i < vec_num; i++)
     {
@@ -71,18 +70,18 @@ void fft_obs_matrix(float32_t* data, arm_rfft_fast_instance_f32* fft_buf, uint16
             placing the output as (buffer + i*(vec_len/2 + 1)) allows us to 
             condense the matrix without leaving any extra space because of
             how real ffts work. */
-    arm_rfft_fast_f32(fft_buf, data + i*vec_len, args->fft_out+ i*(vec_num/2+1), args->ifftFlag);
+    arm_rfft_fast_f32(fft_data, data + i*vec_len, args->fft_out+ i*(vec_num/2+1), args->ifftFlag);
     }	
 }
 
-void cov(float32_t* inp_buf, float32_t* cov_mat, float32_t* means, uint16_t vec_len, uint16_t vec_num)
+void cov(float32_t* input_data, float32_t* cov_mat, float32_t* means, uint16_t vec_len, uint16_t vec_num)
 {
     for (int16_t i = 0; i < vec_len; i++) {
         for (int16_t j = i; j < vec_len; j++) {
             // calculate covariance between two vectors
             cov_mat[i*vec_len + j] = 0;
             for (int16_t k = 0; k < vec_num; k++) {
-                cov_mat[i*vec_len + j] += (inp_buf[k*vec_len + i] - means[i]) * (inp_buf[k*vec_len + j] - means[j]);
+                cov_mat[i*vec_len + j] += (input_data[k*vec_len + i] - means[i]) * (input_data[k*vec_len + j] - means[j]);
             }
             cov_mat[i*vec_len + j] /= vec_num - 1;
             if (i != j) {
@@ -98,10 +97,10 @@ void fft_pca(struct fft_pca_args* args)
     uint16_t vec_num=args->vec_num; 
     
     arm_matrix_instance_f32 mat = args->eig_input->targ_mat;
-    arm_rfft_fast_instance_f32 fft_buf=args->fft_buf;
-    float32_t input_data=args->fft_data;
+    arm_rfft_fast_instance_f32 fft_data = args->fft_data;
+    float32_t input_data = args->data;
 
-    fft_obs_matrix(&input_data, &fft_buf, vec_len, vec_num, args);
+    fft_obs_matrix(&input_data, &fft_data, vec_len, vec_num, args);
    
     vec_len = (vec_len/2 + 1); // since data is real, vectors after fft are length n/2 + 1
     
@@ -128,7 +127,7 @@ void main(){
     uint8_t ifftFlag = 0;
     uint8_t bits = 4;
 
-    initialize(data, &args, &eig_input, output_buffer, &matrix_buffer, input_data, vec_len, vec_num, &fft_data, fft_len, fft_out, ifftFlag, cov_buffer, cov_mat_means, eig_buffer, bits);
+    initialize(tmp, data, &eig_input, &args, output_buffer, &matrix_buffer, input_data, vec_len, vec_num, &fft_data, fft_len, fft_out, ifftFlag, cov_buffer, cov_mat_means, eig_buffer, bits);
     fft_pca(&args);
 }
 
